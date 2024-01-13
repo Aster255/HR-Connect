702025c0e327a717477732099dc03151a1cd9a7e
Paul Adrian Alcos <intpalcos@rush.ph>
2024-01-13 15:29:08 +0800

    migration bug fix

--- database/migrations/2024_01_12_191330_create_employee_information_table.php
+++ database/migrations/2024_01_12_191330_create_employee_information_table.php
@@ -33,6 +33,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('employee_information');
+        Schema::dropIfExists('employee_informations');
     }
 };
